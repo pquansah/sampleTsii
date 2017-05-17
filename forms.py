1ac98ecc40bcaffe7add5095e45818e88fe4a239
Yoofi Quansah <yquansah@usc.edu>
2017-05-17 11:48:32 -0700

    comments added and more to come

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, IntegerField, BooleanField
-from wtforms.validators import Email, InputRequired, DataRequired, Length
+from wtforms.validators import Email, InputRequired, DataRequired, Length, Optional
 
 
 class ApplicantForm(FlaskForm):
@@ -19,3 +19,14 @@ class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[Email(), InputRequired()])
 	password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=27)])
 	submit = SubmitField('Login')
+
+class EditForm(FlaskForm):
+	fname = StringField('First Name', validators=[Optional()])
+	lname = StringField('Last Name', validators=[Optional()])
+	email = StringField('Email', validators=[Email(), Optional()])
+	age = IntegerField('Age', validators=[Optional()])
+	city = StringField('City', validators=[Optional()])
+	state = StringField('State', validators=[Optional()])
+	zip_code = StringField('Zip Code', validators=[Optional()])
+	phone_number = StringField('Phone Number', validators=[Optional()])
+	submit = SubmitField('Submit Edits')
--- views.py
+++ views.py
@@ -1,28 +1,55 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
-from forms import ApplicantForm, LoginForm
+from forms import ApplicantForm, LoginForm, EditForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bootstrap import Bootstrap
 
+
+# initializes app for Flask
 app = Flask(__name__)
+
+# initializes Bootstrap for app, frontend purposes
 Bootstrap(app)
+
+# MongoDB Database name sampletsii and initialization
 app.config['MONGOALCHEMY_DATABASE'] = 'sampletsii'
 app.config['MONGO_URI'] = 'mongodb://tsii:Qazyhnol9@ds161890.mlab.com:61890/sampletsii'
 app.config['SECRET_KEY'] = '435897348y348f3784hf7'
 
+
+# initialize database for app using mongoDB
 mongo = PyMongo(app)
+
+# list of diseases (temporary, will go in database soon)
 diseases = ['Acne', 'AIDS', 'Alopecia Areata', 'Aneurysm', 'Androgenetic Alopecia', 'Angina', 'Asthma', 'Atherosclerosis',
 'ADHD', 'ASD', 'Autoimmune Disease', 'Blood clots', 'Brain Fog']
 
 
+# edit page, where user can edit information that he or she wants to about him or herself
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+	edit_form = EditForm()
+
+	if edit_form.validate_on_submit():
+		pass
+
+
+	return render_template('edit.html', edit_form=edit_form, diseases=request.args.getlist('diseases_names'))
+
+# login page, user will need to provide a email and password to login the pages
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	# login form with email and password field
 	login_form = LoginForm()
 	message = None
 	if login_form.validate_on_submit():
+
+		# if login successfully submits with no errors, pull the email and password and check against database
 		email = login_form.email.data
 		password = login_form.password.data
 		person = mongo.db.people.find({"email" : email})
+
+		# this for loop is to retrieve information from the person so it can be used for display on their profile
 		for sample in person:
 			diseases = sample['health metrics']
 			firstname = sample['fname']
@@ -38,15 +65,18 @@ def login():
 
 	return render_template('login.html', login_form=login_form, message=message)
 
-
+# home page, user can either login or signup
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 	return render_template('home.html')
 
+# signup page, in order for the user to have a successful signup, they would need to provide input for all fields
 @app.route('/signup', methods=['GET', 'POST'])
 def index():
 	applicant_form = ApplicantForm()
+
+	# if the form is successfully processed, meaning all fields are filled out, we will add the user to the database
 	if applicant_form.validate_on_submit():
 		fname = applicant_form.fname.data
 		lname = applicant_form.lname.data
@@ -65,7 +95,7 @@ def index():
 
 	return render_template('signup.html', applicant_form=applicant_form, diseases=diseases)
 
-
+# user can look at basic information of other users that have similar symptons as they do
 @app.route('/personalInfo', methods=['GET', 'POST'])
 def personalInfo():
 	person = request.args.get('samplePerson')
@@ -82,27 +112,21 @@ def personalInfo():
 	return render_template('personalInfo.html', poi=person, age=ageOfPerson, testimonial=actualTestimonial, image=img, city=city, state=state)
 
 
-
+# information page for user, so they will be able to see every user from the database that have similar symptons as they do
 @app.route('/info', methods=['GET', 'POST'])
 def info():
 	diseases_names = request.args.getlist('diseases_names') # includes the list of diseases that the user entered into the index page
 	people = mongo.db.people
 	disease_dict = {}
-	# list_of_queries =[]
-	# for disease in diseases_names:
-	# 	queryResult = people.find({"health metrics" : {"$all" : disease}})
-	# 	list_of_queries.append(queryResult)
 
 	for disease in diseases_names:
 		queryResult = people.find({"health metrics" : {"$all" : [disease]}})
 		disease_dict[disease] = queryResult
 
 
-
-	# for atom in sample:
-	# 	print atom['name']
 	return render_template('info.html', firstname=request.args.get('firstname'), lastname=request.args.get('lastname'), age=request.args.get('age'), diseases_names=request.args.getlist('diseases_names'), qR=disease_dict)
 
+# page user sees when he or she successfully logs in or signs up
 @app.route('/user', methods=['GET', 'POST'])
 def user():
 	firstname = request.args.get('firstname')
